8fc53bdc1fdf2aed3a66db7abb4275e8acb2ce9a
isagers <isabel_gomez7@yahoo.com>
2014-10-29 11:12:03 +0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/libs/Gameplay.m
@@ -0,0 +1,40 @@
+//
+//  Gameplay.m
+//  PeevedPenguins
+//
+//  Created by Isabel Gomez on 10/29/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Gameplay.h"
+
+@implementation Gameplay{
+    CCPhysicsNode *_physicsNode;
+    CCNode *_catapultArm;
+}
+
+-(void)didLoadFromCCB{
+    self.userInteractionEnabled = TRUE;
+}
+
+-(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event{
+    [self launchPenguin];
+}
+
+-(void)launchPenguin{
+    //load penguin ccb from spritebuilder
+    CCNode* penguin = [CCBReader load:@"Penguin"];
+    
+    //position pengiun at bowl of catapult
+    penguin.position = ccpAdd(_catapultArm.position, ccp(16, 50));
+    
+    //add penguin to physics Node
+    [_physicsNode addChild:penguin];
+    
+    //manually create and apply force to launch penguin
+    CGPoint launchDirection = ccp(1, 0);
+    CGPoint force = ccpMult(launchDirection, 8000);
+    [penguin.physicsBody applyForce:force];
+}
+
+@end
