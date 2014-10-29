a63e35744c27ff2d3e49b49fcc93afde3e821484
isagers <isabel_gomez7@yahoo.com>
2014-10-29 11:29:07 +0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/libs/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/libs/Gameplay.m
@@ -41,7 +41,8 @@
     [penguin.physicsBody applyForce:force];
     
     //camera follow
-    self.position = ccp(0,0);
+    // ensure followed object is in visible are when starting
+    self.position = ccp(0, 0);
     CCActionFollow *follow = [CCActionFollow actionWithTarget:penguin worldBoundary:self.boundingBox];
     [self runAction:follow];
 }
